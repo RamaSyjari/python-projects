31932cb90de15a139d03fe178ab317a8c132a735
Rama Syjari <133598953+RamaSyjari@users.noreply.github.com>
2024-10-28 13:33:02 -0700

    Add files via upload

---
+++ Project 1.py
@@ -0,0 +1,4 @@
+print ("Welcom to the youtube Channel Name Generator")
+nikename = input ("What is nikename \n")
+channel = input ("what is your channel about? \n")
+print ("you chould name channel about"+ "(" + nikename + " with " + channel + ")")
